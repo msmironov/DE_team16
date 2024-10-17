901041f909c7f7844aabee66970f34a607bea1ed
kimvkemenade <k.m.v.kemenade@student.tue.nl>
2024-10-17 12:01:54 +0200

    Creating gitignore file for the dataset csv

---
+++ assignment_1/prediction_api/model.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+
+
+
+df = pd.read_csv(path)
+
