efddf7214ba51e2492a88eabbcc2b0510f016225
kimvkemenade <k.m.v.kemenade@student.tue.nl>
2024-10-17 12:08:15 +0200

    Creating a Random Forest model

--- assignment_1/prediction_api/model.py
+++ assignment_1/prediction_api/model.py
@@ -6,7 +6,29 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
+# Load the data from the github repository
+df = pd.read_csv("disease_diagnosis.csv")
 
+# Creating the predictor variables and target variable
+X = df[['Age', 'Heart_Rate_bpm', 'Body_Temperature_C', 'Oxygen_Saturation_%']]
+y = df['Diagnosis']
 
-df = pd.read_csv(path)
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+# Train a Random Forest Classifier
+model = RandomForestClassifier(random_state=42)
+model.fit(X_train, y_train)
+
+# Make predictions
+y_pred = model.predict(X_test)
+
+# This code is just for seeing the accuracy, but not necessary for the assignment
+# Accuracy of the model is 61%
+# # Evaluate the model
+# accuracy = accuracy_score(y_test, y_pred)
+# report = classification_report(y_test, y_pred)
+
+# print(f'Accuracy: {accuracy:.2f}')
+# print('Classification Report:')
+# print(report)
