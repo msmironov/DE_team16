f54772ab7761ba91d8a7f4965754409dec50d112
Gunel Nabiyeva <99348971+Gunelmaas@users.noreply.github.com>
2024-10-20 19:27:13 +0200

    Update app.py

--- assignment_1/prediction_api/app.py
+++ assignment_1/prediction_api/app.py
@@ -1,18 +1,16 @@
-import os
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from joblib import load
 
 app = Flask(__name__)
-app.config["DEBUG"] = True
-model = load('model.joblib')
+model = load('model.joblib')  # Make sure the path is correct depending on where you deploy this
 
-
-@app.route('/diagnose_disease', methods=["GET", "POST"])
-
+@app.route('/diagnose_disease', methods=['GET', 'POST'])
 def predict():
-    symptoms = request.json
-    prediction = model.predict([symptoms.values()])
-    return jsonify({'diagnosis': prediction[0]})
+    if request.method == 'POST':
+        symptoms = request.json
+        prediction = model.predict([symptoms.values()])
+        return jsonify({'diagnosis': prediction[0]})
+    return jsonify({"message": "Method not allowed"}), 405
 
-if __name__ == '__main__':
-    app.run(port=int(os.environ.get("PORT", 5000)), host='0.0.0.0', debug=True)
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
