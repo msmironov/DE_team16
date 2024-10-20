8531ae5f5c067ba2e6d2e89b76ed9118224cde4f
Gunel Nabiyeva <99348971+Gunelmaas@users.noreply.github.com>
2024-10-20 22:28:36 +0200

    Update app.py: handling blood-pressure variable

--- assignment_1/prediction_ui/app.py
+++ assignment_1/prediction_ui/app.py
@@ -6,37 +6,49 @@ import requests
 from flask import Flask, request, render_template, jsonify
 
 # Flask constructor
-app = Flask(__name__)
+app = Flask(__name__, template_folder='.')
 
 
 # A decorator used to tell the application
-# which URL is associated function
+# which URL is associated with the function
 @app.route('/diagnose_disease', methods=["GET", "POST"])
 def diagnose():
     if request.method == "GET":
         return render_template("input_form.html")
 
+
     elif request.method == "POST":
-        prediction_input = [
-            {
-                "Gender": request.form.get("Gender"),  # getting input with name = ntp in HTML form
-                "Age": int(request.form.get("Age")),  # getting input with name = pgc in HTML form
+        try:
+            prediction_input = {
+
+                "Gender": request.form.get("Gender"),
+                "Age": int(request.form.get("Age")),
                 "Symptom_1": request.form.get("Symptom_1"),
                 "Symptom_2": request.form.get("Symptom_2"),
                 "Symptom_3": request.form.get("Symptom_3"),
                 "Heart_Rate_bpm": int(request.form.get("Heart_Rate_bpm")),
                 "Body_Temperature_C": float(request.form.get("Body_Temperature_C")),
-                "Blood_Pressure_mmHg": float(request.form.get("Blood_Pressure_mmHg")),
+                "Blood_Pressure_mmHg": request.form.get("Blood_Pressure_mmHg"),
                 "Oxygen_Saturation_%": int(request.form.get("Oxygen_Saturation_%"))
+
             }
-        ]
+
+            # Handle blood pressure
+            bp = prediction_input["Blood_Pressure_mmHg"].split('/')
+            if len(bp) == 2:
+                systolic_bp = float(bp[0])
+                diastolic_bp = float(bp[1])
+                prediction_input["Systolic_BP"] = systolic_bp
+                prediction_input["Diastolic_BP"] = diastolic_bp
+
+            else:
+                return jsonify(message="Invalid blood pressure format. Please enter as 'Systolic/Diastolic'."), 400
+
+
+        except ValueError as e:
+            return jsonify(message=f"Error processing input data: {str(e)}"), 400
 
         logging.debug("Prediction input : %s", prediction_input)
-
-        # use requests library to execute the prediction service API by sending an HTTP POST request
-        # use an environment variable to find the value of the diabetes prediction API
-        # json.dumps() function will convert a subset of Python objects into a json string.
-        # json.loads() method can be used to parse a valid JSON string and convert it into a Python Dictionary.
         predictor_api_url = os.environ['PREDICTOR_API']
         res = requests.post(predictor_api_url, json=json.loads(json.dumps(prediction_input)))
 
@@ -47,8 +59,8 @@ def diagnose():
 
     else:
         return jsonify(message="Method Not Allowed"), 405  # The 405 Method Not Allowed should be used to indicate
-    # that our app that does not allow the users to perform any other HTTP method (e.g., PUT and  DELETE) for
-    # '/checkdiabetes' path
+
+
 
 
 # The code within this conditional block will only run the python file is executed as a
