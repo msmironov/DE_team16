8486047f6f1d96c09a241c15fbaa5056ebcd7127
Gunel Nabiyeva <99348971+Gunelmaas@users.noreply.github.com>
2024-10-21 19:18:35 +0200

    Update app.py

--- assignment_1/prediction_api/app.py
+++ assignment_1/prediction_api/app.py
@@ -4,7 +4,7 @@ from joblib import load
 app = Flask(__name__)
 model = load('model.joblib')  # Make sure the path is correct depending on where you deploy this
 
-@app.route('/diagnose_disease', methods=['GET', 'POST'])
+@app.route('/diagnose_disease/', methods=['GET', 'POST'])
 def predict():
     if request.method == 'POST':
         symptoms = request.json()
