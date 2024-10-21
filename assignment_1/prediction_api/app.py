6a9128eff74eb7a01c1946eef8899d0abd5f4a4d
Gunel Nabiyeva <99348971+Gunelmaas@users.noreply.github.com>
2024-10-21 21:14:11 +0200

    Update app.py

--- assignment_1/prediction_api/app.py
+++ assignment_1/prediction_api/app.py
@@ -1,18 +1,24 @@
+import os
 from flask import Flask, request, jsonify
-from joblib import load
 from disease_predictor import DiseasePredictor
-dp = DiseasePredictor()
 
 app = Flask(__name__)
-model = load('model.joblib')  # Make sure the path is correct depending on where you deploy this
 
-@app.route('/diagnose_disease/', methods=['GET', 'POST'])
+# Instantiate the predictor
+dp = DiseasePredictor()
+
+@app.route('/predict_disease/', methods=['POST'])
 def predict():
-    if request.method == 'POST':
-        symptoms = request.json()
-        prediction = model.predict([symptoms.values()])
-        return jsonify({'diagnosis': prediction[0]})
-    return jsonify({"message": "Method not allowed"}), 405
+    try:
+        prediction_input = request.get_json()
+        if not prediction_input:
+            return jsonify({'message': 'No input data provided'}), 400
 
-if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+        prediction = dp.predict_single_record(prediction_input)
+        return jsonify({'result': prediction}), 200
+
+    except Exception as e:
+        return jsonify({'message': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(port=int(os.environ.get("PORT", 5000)), host='0.0.0.0', debug=True)
