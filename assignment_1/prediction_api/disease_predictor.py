b4a840a7ec1236dcb049d0deb2fa48c5aa8b2083
msmironov <159449062+msmironov@users.noreply.github.com>
2024-10-21 23:25:53 +0200

    Update disease_predictor.py
    Update the 'load_model' part to fit with the Sklearn model

--- assignment_1/prediction_api/disease_predictor.py
+++ assignment_1/prediction_api/disease_predictor.py
@@ -3,9 +3,9 @@ import os
 
 import pandas as pd
 from flask import jsonify
-from keras.models import load_model
 import logging
 from io import StringIO
+import joblib
 
 
 class DiseasePredictor:
@@ -18,7 +18,7 @@ class DiseasePredictor:
             try:
                 model_repo = os.environ['MODEL_REPO']
                 file_path = os.path.join(model_repo, "model.joblib")
-                self.model = load_model(file_path)
+                self.model = joblib.load(file_path)
             except KeyError:
                 print("MODEL_REPO is undefined")
                 self.model = load_model('model.py')
