0c8769fe0ab3adcbd34e34b4cc5629d9876c00a9
darantza <darantza01@gmail.com>
2024-10-21 12:52:36 +0200

    Update app.py

--- assignment_1/prediction_api/app.py
+++ assignment_1/prediction_api/app.py
@@ -7,7 +7,7 @@ model = load('model.joblib')  # Make sure the path is correct depending on where
 @app.route('/diagnose_disease', methods=['GET', 'POST'])
 def predict():
     if request.method == 'POST':
-        symptoms = request.json
+        symptoms = request.json()
         prediction = model.predict([symptoms.values()])
         return jsonify({'diagnosis': prediction[0]})
     return jsonify({"message": "Method not allowed"}), 405
