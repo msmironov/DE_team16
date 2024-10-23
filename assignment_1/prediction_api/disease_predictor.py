722947fc7bfb45407f55fe433dc3298aac8a899b
msmironov <159449062+msmironov@users.noreply.github.com>
2024-10-23 10:01:52 +0200

    Update disease_predictor.py

--- assignment_1/prediction_api/disease_predictor.py
+++ assignment_1/prediction_api/disease_predictor.py
@@ -13,6 +13,7 @@ class DiseasePredictor:
         self.model = None
 
     def predict_single_record(self, prediction_input):
+        logging.debug(prediction_input)
         if self.model is None:
             try:
                 model_repo = os.environ['MODEL_REPO']
@@ -20,10 +21,12 @@ class DiseasePredictor:
                 self.model = joblib.load(file_path)
             except KeyError:
                 print("MODEL_REPO is undefined")
-                self.model = load_model('model.py')
+                self.model = joblib.load('model.joblib')
 
-        df = pd.DataFrame(prediction_input)
+        df = pd.read_json(StringIO(json.dumps(prediction_input)), orient='records')
         y_pred = self.model.predict(df)
+        logging.info(y_pred[0])
         status = y_pred[0]
+        logging.info(type(status[0]))
         # return the prediction outcome as a json message. 200 is HTTP status code 200, indicating successful completion
-        return jsonify({'result': str(status)}), 200
+        return jsonify({'result': str(status[0])}), 200
