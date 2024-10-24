ab30f58b19af289cf368dfc8bcd3576ce425159f
msmironov <159449062+msmironov@users.noreply.github.com>
2024-10-24 15:11:55 +0200

    Update model_trainer.py

--- assignment_1/training_api/resources/model_trainer.py
+++ assignment_1/training_api/resources/model_trainer.py
@@ -7,8 +7,31 @@ import joblib
 
 def train(dataset):
     # split into input (X) and output (Y) variables
-    X = dataset[:, 0:8]
-    y = dataset[:, 8]
+    y = dataset['Diagnosis']
+    X = dataset.drop(columns=['Patient_ID', 'Diagnosis', 'Severity', 'Treatment_Plan'])
+
+    # Transform Blood Pressure 
+    systolic_bp = X["Blood_Pressure_mmHg"].apply(lambda x: float(x.split('/')[0]))
+    diastolic_bp = X["Blood_Pressure_mmHg"].apply(lambda x: float(x.split('/')[1]))
+    X["Systolic_BP"] = systolic_bp
+    X["Diastolic_BP"] = diastolic_bp
+
+    X=X.drop(columns='Blood_Pressure_mmHg')
+
+#Encode Categorical Variables 
+    category_map={'Fatigue': 0, 'Sore throat': 1, 'Body ache': 2, 'Shortness of breath': 3,
+               'Runny nose': 4, 'Cough': 5, 'Fever': 6, 'Headache': 7}
+
+    X['Symptom_1']=X['Symptom_1'].map(category_map)
+    X['Symptom_2']=X['Symptom_2'].map(category_map)
+    X['Symptom_3']=X['Symptom_3'].map(category_map)
+
+    ohe=OneHotEncoder(drop='if_binary', sparse_output=False)
+    X_ohe=X[['Gender', 'Symptom_1', 'Symptom_2', 'Symptom_3']]
+    X_ohe=pd.DataFrame(ohe.fit_transform(X_ohe), columns=ohe.get_feature_names_out())
+
+    X=X.drop(columns=['Gender', 'Symptom_1', 'Symptom_2', 'Symptom_3'])
+    X=pd.merge(X, X_ohe, how='inner', left_index=True, right_index=True)
 
   # Train a Random Forest Classifier
     model = RandomForestClassifier(random_state=42)
