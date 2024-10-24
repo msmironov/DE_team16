43ee6ccf02861b42702a33d0aeb518b90a3e7cfa
msmironov <159449062+msmironov@users.noreply.github.com>
2024-10-24 16:03:27 +0200

    Update model_trainer.py

--- assignment_1/training_api/resources/model_trainer.py
+++ assignment_1/training_api/resources/model_trainer.py
@@ -54,10 +54,10 @@ def train(dataset):
     # Saving model in a given location provided as an env. variable
     model_repo = os.environ['MODEL_REPO']
     if model_repo:
-        file_path = os.path.join(model_repo)
-        joblib.dump(file_path, "model.joblib")
+        file_path = os.path.join(model_repo, "model.joblib")
+        joblib.dump(model, file_path)
         logging.info("Saved the model to the location : " + model_repo)
         return jsonify(text_out), 200
     else:
-        joblib.dump("model.joblib")
+        joblib.dump(model, "model.joblib")
         return jsonify({'message': 'The model was saved locally.'}), 200
