f2fdab18b50954dd6ceea8d2826c99ed468fc599
msmironov <159449062+msmironov@users.noreply.github.com>
2024-10-28 15:06:08 +0100

    Update disease_predictor.py

--- assignment_1/prediction_api/disease_predictor.py
+++ assignment_1/prediction_api/disease_predictor.py
@@ -12,6 +12,17 @@ class DiseasePredictor:
     def __init__(self):
         self.model = None
 
+    def download_model(self):
+        project_id = os.environ.get('PROJECT_ID', 'Specified environment variable is not set.')
+        model_repo = os.environ.get('MODEL_REPO', 'Specified environment variable is not set.')
+        model_name = os.environ.get('MODEL_NAME', 'Specified environment variable is not set.')
+        client = storage.Client(project=project_id)
+        bucket = client.bucket(model_repo)
+        blob = bucket.blob(model_name)
+        blob.download_to_filename('local_model.joblib')
+        self.model = load_model('local_model.joblib')
+        return jsonify({'message': " the model was downloaded"}), 200
+
     def predict_single_record(self, prediction_input):
         logging.debug(prediction_input)
         if self.model is None:
